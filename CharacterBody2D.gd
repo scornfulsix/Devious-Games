df513be113d8441fc1fea9f58d4e7271987183a1
TheCanner1 <160679514+TheCanner1@users.noreply.github.com>
2024-02-22 17:54:20 -0500

    added downward momentum on down press

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+@export var FRICTION = 800
+const SLIDE_VELOCITY = -300
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -12,14 +14,18 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+	
+	
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+		
+	if Input.is_action_just_pressed("ui_down") and not is_on_floor(): #handles downward momentum on press
+		velocity.y =  JUMP_VELOCITY + 600
+		
 	var direction = Input.get_axis("Left", "Right")
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
